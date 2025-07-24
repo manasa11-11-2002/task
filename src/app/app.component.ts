9fe61d136ca94edd6ccb55258ffea93207bf5862
manasa11-11-2002 <2000031866cse@gmail.com>
2025-07-24 16:25:59 +0530

    new title updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { NavbarComponent } from './navbar/navbar.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'task';
+  title = 'feature-branch-task';
 }
